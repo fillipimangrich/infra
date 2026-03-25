1a75c4c2338d89b00767b1c7628597d07d0ce4a2
fillipimangrich <costa.mangrich@gmail.com>
2026-03-25 01:36:33 -0300

    feat: instalar argocd via helm na camada 2-apps

--- environments/management/2-apps/main.tf
+++ environments/management/2-apps/main.tf
@@ -7,5 +7,55 @@ terraform {
     }
   }
 
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+
   required_version = ">= 1.9"
+}
+
+data "terraform_remote_state" "infra" {
+  backend = "remote"
+
+  config = {
+    organization = "Mangrich"
+    workspaces = {
+      name = "management-infra"
+    }
+  }
+}
+
+locals {
+  kubeconfig = yamldecode(data.terraform_remote_state.infra.outputs.kubeconfig)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = local.kubeconfig.clusters[0].cluster.server
+    cluster_ca_certificate = base64decode(local.kubeconfig.clusters[0].cluster["certificate-authority-data"])
+    client_certificate     = base64decode(local.kubeconfig.users[0].user["client-certificate-data"])
+    client_key             = base64decode(local.kubeconfig.users[0].user["client-key-data"])
+  }
+}
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "7.8.23"
+
+  values = [<<-YAML
+    server:
+      service:
+        type: ClusterIP
+    configs:
+      params:
+        server.insecure: true
+  YAML
+  ]
 }
